bb1cfaf65d4e50aec735068aa9d77da589d547fb
Jimmy Figueroa <jimmyfigueroa@me.com>
2017-01-25 06:40:04 -0600

    added convert index service

--- app/controllers/convert_controller.rb
+++ app/controllers/convert_controller.rb
@@ -1,7 +1,14 @@
+require './lib/romanos'
+
 class ConvertController < ApplicationController
 
   def index
-    msg = {"return":"hola", "code":200}
+    msg = {:return => "nada de convertir", :code => 404 }
+    can_convert = convert params[:arabigo_in] rescue false
+    if can_convert
+     msg[:return] = convert params[:arabigo_in]
+     msg[:code] = 200
+    end
     render json: msg
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :convert
 
-  #get 'states/:country_id/index', :controller=> 'state', :action=>'index' 
-  #get 'cities/:country_id/:state_id/index', :controller=> 'city', :action=>'index'
-  #get ':id/data_package/', :controller=> 'data_package', :action=>'show'
+  #get ':arabigo_in/convert', :controller=> 'convert', :action => 'show'
+
 end
